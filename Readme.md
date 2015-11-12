de0f0797529fe0507f23a640f4cc80cf31dfe663
Artem Sapegin <artem@sapegin.ru>
2015-11-12 22:49:51 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,78 @@
+# expect-react-shallow
+
+[![Build Status](https://travis-ci.org/sapegin/expect-react-shallow.svg)](https://travis-ci.org/sapegin/expect-react-shallow)
+
+A [Chai’s](http://chaijs.com) expect like API wrapper for [unexpected-react-shallow](https://github.com/bruderstein/unexpected-react-shallow). It also accepts JSX instead of ShallowRenderer instance.
+
+![expect-react-shallow](https://s3.amazonaws.com/f.cl.ly/items/1M3a3B163l392G1N3v25/Screen%20Shot%202015-11-12%20at%2022.39.57.png)
+
+
+## Installation
+
+```shell
+npm install --save-dev expect-react-shallow
+```
+
+
+## Usage
+
+```js
+import React from 'react';
+import { expect } from 'mocha';
+import expectReactShallow from 'expect-react-shallow';
+
+class TestComponent extends React.Component {
+	render() {
+		return <div className="foo">Hello React!</div>;
+	}
+}
+
+describe('TestComponent', () => {
+
+  it('works', () => {
+	expectReactShallow(<TestComponent/>).to.have.rendered(<div>Hello React!</div>);  // OK
+
+	expectReactShallow(<TestComponent/>).to.have.exactly.rendered(<div className="foo">Hello React!</div>)  // OK
+
+	expectReactShallow(<TestComponent/>).to.have.exactly.rendered(<div/>)  // Error
+
+	expectReactShallow(<TestComponent/>).to.contain(<div/>)  // OK
+  });
+
+});
+```
+
+
+## API
+
+```js
+expectReactShallow(ReactComponent|JSX).to.have.rendered(ReactComponent|JSX);
+expectReactShallow(ReactComponent|JSX).to.have.rendered.with.all.children(ReactComponent|JSX);
+expectReactShallow(ReactComponent|JSX).to.have.exactly.rendered(ReactComponent|JSX);
+expectReactShallow(ReactComponent|JSX).to.contain(ReactComponent|JSX);
+expectReactShallow(ReactComponent|JSX).to.contain.exactly(ReactComponent|JSX);
+```
+
+See details in [unexpected-react-shallow docs](https://github.com/bruderstein/unexpected-react-shallow#assertions).
+
+
+## Changelog
+
+The changelog can be found in the [Changelog.md](Changelog.md) file.
+
+
+## Contributing
+
+Everyone is welcome to contribute. Please take a moment to review the [contributing guidelines](Contributing.md).
+
+
+## Author
+
+* [Artem Sapegin](http://sapegin.me)
+
+
+---
+
+## License
+
+The MIT License, see the included [License.md](License.md) file.
---
+++ test/test.js
@@ -0,0 +1,104 @@
+import { expect } from 'chai';
+import React from 'react';
+
+import expectReactShallow from '../lib';
+
+class TestComponent extends React.Component {
+	render() {
+		return (
+			<div className="foo">Hello React!</div>
+		);
+	}
+}
+
+describe('expectReactShallow', () => {
+
+	describe('API', () => {
+
+		it('expectReactShallow() should return all API methods ', () => {
+			let api = expectReactShallow(<TestComponent/>);
+			expect(api.to.have.rendered).to.be.a.func;
+			expect(api.to.have.rendered.with.all.children).to.be.a.func;
+			expect(api.to.have.exactly.rendered).to.be.a.func;
+			expect(api.to.contain).to.be.a.func;
+			expect(api.to.contain.exactly).to.be.a.func;
+		});
+
+	});
+
+	describe('expectReactShallow().to.have.rendered()', () => {
+
+		it('should not throw an error if the JSX is equal', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.have.rendered(<div>Hello React!</div>)
+			).to.not.throw();
+		});
+		it('should throw an error if the JSX is not equal', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.have.rendered(<div>Hello world!</div>)
+			).to.throw();
+		});
+
+	});
+
+	describe('expectReactShallow().to.have.exactly.rendered()', () => {
+
+		it('should not throw an error if the JSX is exactly equal', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.have.exactly.rendered(<div className="foo">Hello React!</div>)
+			).to.not.throw();
+		});
+		it('should throw an error if the JSX is not exactly equal', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.have.exactly.rendered(<div/>)
+			).to.throw();
+		});
+
+	});
+
+	describe('expectReactShallow().to.have.rendered.with.all.children()', () => {
+
+		it('should not throw an error if the JSX is equal with all children', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.have.rendered.with.all.children(<div>Hello React!</div>)
+			).to.not.throw();
+		});
+		it('should throw an error if the JSX is not equal with all children', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.have.rendered.with.all.children(<div/>)
+			).to.throw();
+		});
+
+	});
+
+	describe('expectReactShallow().to.contain()', () => {
+
+		it('should not throw an error if the JSX contains another JSX', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.contain(<div/>)
+			).to.not.throw();
+		});
+		it('should throw an error if the JSX not contains another JSX', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.contain(<span/>)
+			).to.throw();
+		});
+
+	});
+
+	describe('expectReactShallow().to.contain.exactly()', () => {
+
+		it('should not throw an error if the JSX contains exactly another JSX', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.contain.exactly(<div className="foo">Hello React!</div>)
+			).to.not.throw();
+		});
+		it('should throw an error if the JSX not contains exactly another JSX', () => {
+			expect(
+				() => expectReactShallow(<TestComponent/>).to.contain.exactly(<div>Hello React!</div>)
+			).to.throw();
+		});
+
+	});
+
+});
